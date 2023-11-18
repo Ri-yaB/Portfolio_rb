5654413059e228f491bfe582807d977e33e449cc
Riya bansal <103059954+Ri-yaB@users.noreply.github.com>
2023-11-18 12:58:43 +0530

    changes

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -44,7 +44,7 @@ const MovingImg = ({title, img, link}) =>{
 
 const FeaturedArticle =({img, title, time, summary, link}) =>{
     return(
-        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative'>
+        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative dark:bg-dark dark:border-light'>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl' />
             <Link href={link} 
             target="_blank"
@@ -55,7 +55,7 @@ const FeaturedArticle =({img, title, time, summary, link}) =>{
                 <h2 className='capitalize inline-block hover:underline text-2xl font-bold my-2 mt-4'>{title}</h2>
             </Link>
             <p className='text-sm mb-2'>{summary}</p>
-            <span className='text-primary font-semibold'>{time}</span>
+            <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
         </li>
     )
 }
@@ -66,13 +66,13 @@ const Article = ({img, title, date, link}) =>{
         initial={{y:200}}
         whileInView={{y:0, transition:{duration:0.5, ease:"easeInOut"}} }
         viewport={{once:true}}
-        className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'>
+        className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'>
             <Link href={link} target='_blank'>
                 <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
             </Link>
             <MovingImg title={title} img={img} link={link} />
             
-            <span className='text-primary font-semibold pl-4'>{date}</span>
+            <span className='text-primary dark:text-primaryDark font-semibold pl-4'>{date}</span>
 
         </motion.li>
     )
@@ -85,7 +85,7 @@ const articles = () => {
         <title>Riya Bansal | Articles Page</title>
         <meta name="description" content='any description'/>
     </Head>
-    <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
+    <main className='w-full mb-16 flex flex-col dark:text-light items-center justify-center overflow-hidden'>
         <Layout className='pt-16'>
             <AnimatedText text="Words Can Change The World!" className='mb-16' />
             <ul className='grid grid-cols-2 gap-16'>
