1d9d8b33292b519a1aaabdb42e1622fe5519c3ad
Riya bansal <103059954+Ri-yaB@users.noreply.github.com>
2023-11-20 14:50:15 +0530

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,7 +45,7 @@ const NavBar = () => {
     }
 
   return (
-    <header className='w-full px-32 py-8 font-medium flex tems-center justify-between dark:text-light relative'>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8'>
        
        <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
         <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
@@ -94,7 +94,10 @@ const NavBar = () => {
 
         {
             isOpen ?
-            <div className='min-w-[70vw] flex flex-col z-30 justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+            <motion.div 
+            initial={{scale:0, opacity:0, x: "-50%", y:"-50%"}}
+            animate={{scale:1, opacity:1}}
+            className='min-w-[70vw] flex flex-col z-30 justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
         <nav className='flex items-center flex-col justify-center'> 
             <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
             <CustomMobileLink href="/about"title="About"className='' toggle={handleClick} />
@@ -134,7 +137,7 @@ const NavBar = () => {
             </button>
 
         </nav>
-        </div>
+        </motion.div>
             : null
         }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -42,9 +42,9 @@ const about = () => {
         </Head>
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'> 
-            <AnimatedText text="Passion fuels Purpose" className='mb-16' />
-            <div className='grid w-full grid-cols-8 gap-16'>
-                <div className='col-span-3 flex flex-col items-start justify-start'>
+            <AnimatedText text="Passion fuels Purpose" className='mb-16 lg:!text-7xl xs:!text-4xl sm:mb-8' />
+            <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                     <h2 className='nb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About Me</h2>
                 <p className='my-4 font medium justify-center'>
                 As a highly motivated and detail-oriented individual, I am dedicated to achieving success in all of my endeavors. With excellent communication and interpersonal skills, I thrive in team environments and enjoy collaborating with others to reach common goals. My strong work ethic and adaptability allow me to take on new challenges with ease and deliver quality results.
@@ -52,7 +52,7 @@ const about = () => {
 Innovative web developer with a passion for building data-driven web applications that solve complex business challenges. Expertise in Python, SQL, and C language, with a focus on enhancing the user experience through creative and intuitive design.
                 </p>
                 </div>
-                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
                 <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                 <Image src={profilePic} alt="Riya Bansal" 
                 className='w-full h-auto rounded-2xl'
@@ -62,26 +62,26 @@ Innovative web developer with a passion for building data-driven web application
   33vw"
    />
                 </div>
-                <div className='col-span-2 flex flex-col items-end justify-between'>
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                             <AnimatedNumbers value={30} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Satisfied Clients</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Satisfied Clients</h2>
                     </div>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                         <AnimatedNumbers value={20} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Projects Completed</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Projects Completed</h2>
                     </div>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                         <AnimatedNumbers value={3} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75'>Years of Experience</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Years of Experience</h2>
                     </div>
 
 
